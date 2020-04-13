5bce62555b8df15f03154d2336f2cdf081ebb241
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-04-13 16:29:01 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'posts/:id', to: 'posts#show'
+  # get 'posts/:id', to: 'posts#show'
+  #refactored code 
+  resources :posts, only: :show
 end
